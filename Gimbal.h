ea1add3440f6f45aeff9613f29ce50bbdf7515fd
Q-Jackson <quaaludejackson@zerocharlie.com>
2026-06-26 19:47:00 -0500

    Update Gimbal.h

--- Gimbal.h
+++ Gimbal.h
@@ -20,20 +20,20 @@ class Gimbal {
 	static const uint8_t GIMBAL_I2C_ADDR = 0x40;	// I2C bus address of servo controller
 	static const uint8_t SERVO_FREQ = 50;		// typical servo pulse frequency, Hz
 	static constexpr float US_PER_BIT = (1e6/SERVO_FREQ/4096);	// usec per bit @ 12 bit resolution
-	static const uint8_t MOT1_UNIT = 0;		// motor 1 I2C unit number
-	static const uint8_t MOT2_UNIT = 1;		// motor 2 I2C unit number
-	bool gimbal_found;				// whether PWM controller is present
+	static const uint8_t MOT1_UNIT = 0;		  // motor 1 I2C unit number
+	static const uint8_t MOT2_UNIT = 1;		  // motor 2 I2C unit number
+	bool gimbal_found;				              // whether PWM controller is present
 
 	// motor info
 	typedef struct {
 	    float az_scale, el_scale;			// az and el scale: steps (del usec) per degree
-	    uint16_t min, max;				// position limits, usec
-	    uint16_t pos;				// last commanded position, usec
-	    int16_t del_pos;				// change in pos since previous move
-	    bool atmin, atmax;				// (would have been commanded to) limit
-	    uint8_t servo_num;				// I2C bus address 0..15
+	    uint16_t min, max;				    // position limits, usec
+	    uint16_t pos;				          // last commanded position, usec
+	    int16_t del_pos;				      // change in pos since previous move
+	    bool atmin, atmax;				    // (would have been commanded to) limit
+	    uint8_t servo_num;				    // I2C bus address 0..15
 	} MotorInfo;
-	static const uint8_t NMOTORS = 2;		// not easily changed
+	static const uint8_t NMOTORS = 2; // not easily changed
 	MotorInfo motor[NMOTORS];
 
 	// search info
@@ -42,10 +42,10 @@ class Gimbal {
 	static constexpr float MAX_SETTLE = 5.0;	// considered stopped, degs
 	static const uint8_t N_INIT_STEPS = 4;		// number of init_steps
 	static constexpr float CAL_FRAC = 0.333;	// fraction of full range to move for calibration
-							// N.B.: max physical motion must be < 180/CAL_FRAC
-	uint8_t init_step;				// initialization sequencing
-	uint8_t best_azmotor;				// after cal, motor[] index with most effect in az
-	uint32_t last_update;				// millis() time of last move
+							                              // N.B.: max physical motion must be < 180/CAL_FRAC
+	uint8_t init_step;				          // initialization sequencing
+	uint8_t best_azmotor;			        	// after cal, motor[] index with most effect in az
+	uint32_t last_update;				        // millis() time of last move
 	float prevfast_az, prevfast_el;			// previous pointing position
 	float prevstop_az, prevstop_el;			// previous stopped position
 
